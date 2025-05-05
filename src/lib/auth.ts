d249d290761ba47ec1ab04a464e5d9d576df87ea
taishi-yamada <taishi.37.0327@gmail.com>
2025-05-05 20:14:40 +0900

    feat prisma client

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,11 @@
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import EmailProvider from 'next-auth/providers/email';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from './prisma';
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
